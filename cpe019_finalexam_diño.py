1ddb698d3dbf8bde7dd4239969ae6aba6a182caa
NicoleDino <112040512+NicoleDino@users.noreply.github.com>
2023-05-20 01:00:23 +0800

    New updates on cpe019_finalexam_diño.py

--- cpe019_finalexam_diño.py
+++ cpe019_finalexam_diño.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  mwd_model=tf.keras.models.load_model('predict_weather(classification).h5')
-  return mwd_model
-mwd_model=load_model()
+  model=tf.keras.models.load_model('predict_weather(classification).h5')
+  return model
+model=load_model()
 st.write("""
 # Welcome to the Weather Classifier! 
 # Check and predict the weather today!"""
@@ -17,19 +17,19 @@ file=st.file_uploader("Select any weather-related image from your device",type=[
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,mwd_model):
-    size=(64,64)
+def import_and_predict(image_data,model):
+    size=(96,96)
     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
-    prediction=mwd_model.predict(img_reshape)
+    prediction=model.predict(img_reshape)
     return prediction
 if file is None:
     st.text("You may now upload your chosen image file (paste your weather photograph here)")
 else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,mwd_model)
+    prediction=import_and_predict(image,model)
     class_names=['Cloudy', 'Rain', 'Shine', 'Sunrise']
     string="THE WEATHER IS : "+class_names[np.argmax(prediction)]
     st.success(string)
